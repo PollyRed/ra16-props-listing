09c6ec99c6085283884d69cc48976f01ddb6182f
Polina Titova <titova_polina@mail.ru>
2022-06-29 00:35:36 +0300

    Task completed

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -15,22 +15,33 @@ function Listing(props) {
           } else {
             price = `${item.price} ${item.currency_code}`;
           }
+
+          let level;
+          if (item.quantity <= 10) {
+            level = "low";
+          } else if (item.quantity > 20) {
+            level = "high";
+          } else {
+            level = "medium";
+          }
         
           return (
           <div className="item" key={item.listing_id}>
             <div className="item-image">
               <a href={item.url}>
-                <img src={item.url_570xN} alt=""/>
+                <img src={item.MainImage.url_570xN} alt=""/>
               </a>
             </div>
             <div className="item-details">
               <p className="item-title">{title}</p>
               <p className="item-price">{price}</p>
-              <p className="item-quantity level-medium">{item.quantity} left</p>
+              <p className={`item-quantity level-${level}`}>{item.quantity} left</p>
             </div>
           </div>
           );
-        }
+        } 
+
+        return <div></div>;
       })}
     </div>
   );
